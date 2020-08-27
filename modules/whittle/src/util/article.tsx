9da0098f840c24ed42d9a275563a5c422eb2e1e0
Jared Hanson <jred0011@gmail.com>
2020-08-26 18:14:59 -0700

    Made word counter better so read time is more realistic

--- modules/whittle/src/util/article.tsx
+++ modules/whittle/src/util/article.tsx
@@ -1,6 +1,7 @@
 export default class ArticleUtils {
   static calculateReadingTime(content: string): number {
-    let words = content.split(' ')
+    let strippedContent = content.replace(/<[^>]*>?/gm, '')
+    let words = strippedContent.split(' ')
     return Math.round(words.length / 300)
   }
 }
