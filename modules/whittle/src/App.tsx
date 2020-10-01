7c327d459554b40c77c3d7fa397b75fc68a41fd4
Jared Hanson <jred0011@gmail.com>
2020-10-01 00:37:34 -0700

    Added covers page for mobile devices

--- modules/whittle/src/App.tsx
+++ modules/whittle/src/App.tsx
@@ -1,14 +1,43 @@
 import {ConnectedRouter} from 'connected-react-router'
-import React from 'react'
+import React, {useEffect, useState} from 'react'
 import {Provider} from 'react-redux'
+import NoMobilePage from './pages/NoMobilePage'
 import Stack from './stacks'
 import store, {history} from './store'
 
 export default function App() {
+  ///////// Handle dimensions for covers page
+  let [windowDimensions, setWindowDimensions] = useState<{
+    height: number | null
+    width: number | null
+  }>({height: null, width: null})
+
+  const hasWindow = typeof window !== 'undefined'
+  useEffect(() => {
+    function getWindowDimensions() {
+      const width = hasWindow ? window.innerWidth : null
+      const height = hasWindow ? window.innerHeight : null
+      return {
+        width,
+        height,
+      }
+    }
+    if (hasWindow) {
+      const handleResize = () => setWindowDimensions(getWindowDimensions())
+      window.addEventListener('resize', handleResize)
+      return () => window.removeEventListener('resize', handleResize)
+    }
+  }, [hasWindow])
+  ///////// END Handle dimensions for covers page
+
   return (
     <Provider store={store}>
       <ConnectedRouter history={history}>
-        <Stack />
+        {windowDimensions.width !== null && windowDimensions.width >= 768 ? (
+          <Stack />
+        ) : (
+          <NoMobilePage />
+        )}
       </ConnectedRouter>
     </Provider>
   )
